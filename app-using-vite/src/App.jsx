7a8b38b2fec6c7ae736a82a503b8e83b30c8ec25
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-07 19:35:10 +0530

    ..

--- app-using-vite/src/App.jsx
+++ app-using-vite/src/App.jsx
@@ -1,11 +1,21 @@
-import Temp from "./temp"
-
 function App() {
-   return (
+  let counter = 15;
+  const addvalue = () =>{
+    counter++;
+    console.log(counter);
+    
+  }
+  return (
     <>
-      <Temp />
+      <h1>Hello React : </h1>
+      <h2>Counter Value : {counter}</h2>
+      <button
+        onClick={addvalue}
+      >Add Value</button>
+      <br /><br />  
+      <button>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app-using-vite/src/main.jsx
+++ app-using-vite/src/main.jsx
@@ -6,8 +6,5 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <App />
-    <App />
-    <App />
   </React.StrictMode>,
 )
