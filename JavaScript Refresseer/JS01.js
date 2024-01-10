7b62523e21cea02625466c838b23fff4ffbc4d2d
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-10 19:55:54 +0530

    commited Javascript Refresser :

---
+++ JavaScript Refresseer/JS01.js
@@ -0,0 +1,39 @@
+// let a = 23;
+// console.log(a);
+
+// DATA Types
+
+// Number
+// Object
+// Boolean
+// String
+// undefined
+// null
+
+// function fun() {
+//       console.log("Hello Ak");
+// }
+// fun();
+
+// const obj = {
+//       name:"Akash",
+//       surname:"Rathod",
+//       age:"20"
+// }
+// console.log(`My Name Is ${obj.surname} ${obj.name} And My Age Is ${obj.age}`);
+
+
+// const button = document.querySelector('.btn');
+// console.log(button);
+
+// button.addEventListener('click', () => {
+//       console.log("Hello Javascript ");
+// } )
+
+// const button = document.querySelector('.btn');
+// button.addEventListener('click', () => {
+//       alert("You Are In Wrong Website : ")
+//       prompt("enter any one number : ")
+//       const conf = confirm("are you sure : ")
+//       console.log(conf);
+// })
---
+++ JavaScript Refresseer/JS02.js
@@ -0,0 +1,50 @@
+"use strict";
+
+
+
+// console.log("Hello Ak : ");
+
+// for (let index = 0; index < array.length; index++) {
+//       const element = array[index];
+      
+      
+// }
+
+// array.forEach(element => {
+   
+// });
+
+// for (const key in object) {
+//       if (Object.hasOwnProperty.call(object, key)) {
+//             const element = object[key];
+            
+//       }
+// }
+
+// for (const key in object) {
+//       if (Object.hasOwnProperty.call(object, key)) {
+//             const element = object[key];
+            
+//       }
+// }
+
+
+// for (const iterator of object) {
+      
+// }
+
+
+// d = 30;
+// console.log(d);
+
+// setTimeout(() => {
+//       console.log("this is the setTimeOut");
+// }, 1000);
+
+// setInterval(() => {
+//       console.log("this is the setInterval :");
+// }, 1000);
+
+// console.log("hello ak : ");
+
+
