030adce7ae7f220158049cbb802b33fc7b38c778
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-11 21:39:54 +0530

    new commit on myapp/navbar,textform

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-
+import Navbar from "./componants/navbar";
+import TextForm from "./componants/TextForm";
 function App() {
   return (
     <>
+      {/* <Navbar logo = "Hello Ak" />  */}
+      {/* <Navbar logo = "theproteam" />  */}
+      {/* <Navbar logo = "ProTeam" />  */}
 
-
-
+      <Navbar logo="ProTeam" port="Portfolio" />
+      <div className="container my-5">
+        <TextForm heading="Type The Text : Below " />
+      </div>
     </>
   );
 }
---
+++ myapp/src/componants/TextForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function TextForm(props) {
+  function convertToUpeperCase() {
+    console.log("clicked to button");
+    console.log(upperCase);
+    let newText = upperCase.toUpperCase();
+    setUpperCase(newText);
+  }
+
+  function onChangeFun(event) {
+    console.log("on change");
+    setUpperCase(event.target.value);
+  }
+
+  const [upperCase, setUpperCase] = useState("akash");
+  return (
+    <>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          value={upperCase}
+          onChange={onChangeFun}
+        ></textarea>
+      </div>
+      <button onClick={convertToUpeperCase} className="btn btn-primary">
+        convert to upperCase
+      </button>
+      {/* <button className="btn btn-primary">convert to lowerCase</button>
+      <button className="btn btn-primary">convert to Reset</button> */}
+    </>
+  );
+}
+
+export default TextForm;
