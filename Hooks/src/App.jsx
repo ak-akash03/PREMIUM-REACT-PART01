c406ff4dcee0228aca0d305edb17f0a19a2a4bc0
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-20 11:07:06 +0530

    hook app.jsx

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,27 +1,56 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+
+// function App() {
+
+//   // var val = "Hello Ak"
+//   // const changeText = () => {
+//   //   val = "Hello Akash "
+//   //   console.log(val);
+//   // }
+
+//   const [myName, setMyName] = useState("Akash Rajendra Rathod ");
+
+//   const changeName = () => {
+//     setMyName("Sagar Rajendra Rathod")
+
+//   }
+
+//   return (
+//     <>
+//       <h1>{myName}</h1>
+//       <button className="btn" onClick={changeName}>Click Me To Change Text</button>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react'
 
 function App() {
 
-  // var val = "Hello Ak"
-  // const changeText = () => {
-  //   val = "Hello Akash "
-  //   console.log(val);
-  // }
+  const [myName, setMyName] = useState("Hello Ak")
 
-  const [myName, setMyName] = useState("Akash Rajendra Rathod ");
 
-  const changeName = () => {
-    setMyName("Sagar Rajendra Rathod")
-    
+  const changeText = () => {
+    let val = myName
+
+    if (val === "Hello Ak")
+      setMyName("Hello Akash")
+    else
+      setMyName("Hello Ak")
+
   }
 
   return (
     <>
       <h1>{myName}</h1>
-      <button className="btn" onClick={changeName}>Click Me To Change Text</button>
+      <button className='btn' onClick={changeText}>Change Text</button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
