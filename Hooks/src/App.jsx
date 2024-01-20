34be7c7257ada8ce4543c6550199867d63feb60f
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-20 10:58:41 +0530

    ..

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+
 
 function App() {
+
+  // var val = "Hello Ak"
+  // const changeText = () => {
+  //   val = "Hello Akash "
+  //   console.log(val);
+  // }
+
+  const [myName, setMyName] = useState("Akash Rajendra Rathod ");
+
+  const changeName = () => {
+    setMyName("Sagar Rajendra Rathod")
+    
+  }
+
   return (
-    <div>This Is A App Componants : </div>
-  )
+    <>
+      <h1>{myName}</h1>
+      <button className="btn" onClick={changeName}>Click Me To Change Text</button>
+    </>
+  );
 }
 
-export default App
+export default App;
